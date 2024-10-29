18e5fe010652fe2f69828bc46f40b5af8a737fc4
zeineb-saadouli <saadoulizeineb1@gmail.com>
2024-10-29 10:52:49 +0100

    first commit

--- quizz.js
+++ quizz.js
@@ -1,27 +1,24 @@
-document.addEventListener('DOMContentLoaded',function(){
-    function checkAnswer(){
-        const answers=document.getElementsByName('answer');
+document.addEventListener('DOMContentLoaded',function(){//lien entre DOM et js
+    function checkAnswer(){// fonction à exécuter lorsque on clique sur le bouton 
+        const answers=document.getElementsByName('answer');// selectionner toute les balises ayant name= answer
         let score=0;
         let selectedAnswer=null;
-        for (const x of answers){
-            if (x.checked){
-                selectedAnswer = x.value;
-                break;
+        let correctAnswer=["26","Hyper Text Markup Language","3.14"]// le tableau contenant les bonnes réponses 
+        for (const x of answers){// parcourir les réponses de l'utilisateur
+            if (x.checked){// si x  ayant name= answer est sélectionné
+                selectedAnswer=x.value;// la valeur de x
+              if (correctAnswer.includes(selectedAnswer) ){// vérifier si la réponse sélectionnée est dans correctAnswer
+                score+=1;// incrimenter le score
+              } 
+            
             }
         }
-        if (selectedAnswer === null ){
-            alert("sélectionner une réponse");
-            return
-        }
-        switch (selectedAnswer) 
-        {case "26": score+=1;
-        break;
-        case"21": score=0;
-
-        }
-    
+    if (score===0){// tester si le score=0, il renvoie la réponse est fausse
+        alert("la réponse est fausse");
+        
+    }else {
+        alert(" votre score est :"+ score);// si non afficher le score
     }
+    }
+    document.getElementById('btn').addEventListener("click",checkAnswer);
 })
-
-
-
